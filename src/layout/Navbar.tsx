533e17ab3790bd92fb42794adc1c4e590d2edafc
Okky <okky.mstafford@gmail.com>
2026-02-25 11:34:47 +1100

    Resolved MDI error in burger icon

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -94,7 +94,17 @@ export function Navbar({
           aria-label={isMobile ? 'Open menu' : 'Toggle sidebar'}
           aria-expanded={isMobile ? sidebarMobileOpen : undefined}
         >
-          <i className="mdi mdi-menu" aria-hidden />
+          <svg
+            className="navbar-menu-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5em"
+            height="1.5em"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            aria-hidden
+          >
+            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+          </svg>
         </button>
       </div>
       {sidebarCollapsed && !isMobile && (
